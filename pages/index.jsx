8c24bba77de0a7124f661ddce03cdf75100dcb82
Augustine Ugbaja <augustineugbaja97@gmail.com>
2022-07-11 14:35:37 +0100

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import PostCard from "../components/PostCard";
+
+const index = () => {
+  const styles = {};
+  return (
+    <>
+      <Header />
+      <Banner />
+      <PostCard />
+    </>
+  );
+};
+
+export default index;
